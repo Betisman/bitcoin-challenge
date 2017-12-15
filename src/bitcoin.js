d68cadaf69f2789afa07f6378192f4ddc1c242c5
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2017-12-15 01:59:11 +0100

    even shorter approach for maxProfit. And there were a typo with sliced

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -3,10 +3,9 @@ module.exports = () => {
   const maxProfit = prices =>
     prices.reduce((acc, price) => {
       const sliced = acc.sliced.slice(1);
-      const max = Math.max(...acc.sliced);
       return {
         sliced,
-        max: Math.max(max - price, acc.max) || -1,
+        max: Math.max(Math.max(...sliced) - price, acc.max) || -1,
       };
     }, { max: -1, sliced: prices }).max;
 
