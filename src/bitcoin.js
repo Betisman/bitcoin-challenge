920a23f696d4f130e1ec9796a29305504ea21e4f
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2017-12-15 02:06:25 +0100

    there is no need to slice twice, so we do not need to keep the sliced array

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,13 +1,10 @@
 module.exports = () => {
 
   const maxProfit = prices =>
-    prices.reduce((acc, price) => {
-      const sliced = acc.sliced.slice(1);
-      return {
-        sliced,
-        max: Math.max(Math.max(...sliced) - price, acc.max) || -1,
-      };
-    }, { max: -1, sliced: prices }).max;
+    prices.reduce((acc, price) => ({
+        sliced: acc.sliced.slice(1),
+        max: Math.max(Math.max(...acc.sliced) - price, acc.max) || -1,
+      }), { max: -1, sliced: prices }).max;
 
   return { maxProfit };
 };
